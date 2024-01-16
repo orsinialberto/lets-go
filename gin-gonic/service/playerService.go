f29d09fdb96a05f1882e01dccc609514955bfcb5
Alberto Orsini <alberto.orsini@office.mailup.it>
2024-01-16 11:56:57 +0100

    refactor error manage

--- gin-gonic/service/playerService.go
+++ gin-gonic/service/playerService.go
@@ -32,22 +32,31 @@ func PostPlayer(c *gin.Context) {
 		return
 	}
 
-	writePlayer(jsonP)
+	if err := writePlayer(jsonP); err != nil {
+		fmt.Println("Error:", err)
+		c.IndentedJSON(http.StatusBadRequest, "Error writing player")
+		return
+	}
 
 	c.IndentedJSON(http.StatusCreated, p)
 }
 
 func GetPlayer(c *gin.Context) {
-	pId := c.Param("id")
-
-	fmt.Println("Searching player " + pId)
+	id := c.Param("id")
+	fmt.Println("Search player", id)
 
 	var p model.Player
-	err := json.Unmarshal([]byte(readPlayer(pId)), &p)
 
+	player, err := readPlayer(id)
 	if err != nil {
-		fmt.Println("Player not found " + pId)
-		c.IndentedJSON(http.StatusNotFound, "player "+pId+" not found")
+		fmt.Println("Player not found", id)
+		c.IndentedJSON(http.StatusNotFound, "player "+id+" not found")
+		return
+	}
+
+	if err := json.Unmarshal([]byte(player), &p); err != nil {
+		fmt.Println("Error:", err)
+		c.IndentedJSON(http.StatusBadRequest, "error unmarshalling player")
 		return
 	}
 
@@ -56,30 +65,39 @@ func GetPlayer(c *gin.Context) {
 
 func GetPlayers(c *gin.Context) {
 	fmt.Println("Searching players")
-	players := readPlayers()
+
+	players, err := readPlayers()
+	if err != nil {
+		fmt.Println("Error:", err)
+		c.IndentedJSON(http.StatusBadRequest, "error reading players")
+		return
+	}
+
 	c.IndentedJSON(http.StatusCreated, players)
 }
 
-func writePlayer(p string) {
-
+func writePlayer(s string) error {
+	// todo mettere nome file in file di configurazione
 	file, err := os.OpenFile("players.txt", os.O_APPEND|os.O_CREATE|os.O_WRONLY, 0666)
 	if err != nil {
 		fmt.Println("Error:", err)
-		return
+		return err
 	}
 	defer file.Close()
 
-	if _, err := file.WriteString(p + "\n"); err != nil {
+	if _, err := file.WriteString(s + "\n"); err != nil {
 		fmt.Println("Error:", err)
-		return
+		return err
 	}
+
+	return nil
 }
 
-func readPlayer(pId string) string {
+func readPlayer(pId string) (string, error) {
 	file, err := os.Open("players.txt")
 	if err != nil {
 		fmt.Println("Error:", err)
-		return ""
+		return "", err
 	}
 	defer file.Close()
 
@@ -87,22 +105,22 @@ func readPlayer(pId string) string {
 	for scanner.Scan() {
 		line := scanner.Text()
 		if strings.Contains(line, pId) {
-			return line
+			return line, nil
 		}
 	}
 
 	if err := scanner.Err(); err != nil {
-		return ""
+		return "", err
 	}
 
-	return ""
+	return "", nil
 }
 
-func readPlayers() []model.Player {
+func readPlayers() ([]model.Player, error) {
 	file, err := os.Open("players.txt")
 	if err != nil {
 		fmt.Println("Error:", err)
-		return nil
+		return nil, err
 	}
 	defer file.Close()
 
@@ -111,12 +129,12 @@ func readPlayers() []model.Player {
 	for scanner.Scan() {
 		line := scanner.Text()
 		var p model.Player
-		err := json.Unmarshal([]byte(line), &p)
-		if err != nil {
+		if err := json.Unmarshal([]byte(line), &p); err != nil {
 			fmt.Println("Error:", err)
-			return nil
+			return nil, err
 		}
 		players = append(players, p)
 	}
-	return players
+
+	return players, nil
 }
