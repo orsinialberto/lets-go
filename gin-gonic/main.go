08a0454d2a73fd5c454e5b857cf85fca1b37d5d7
Alberto Orsini <alberto.orsini@office.mailup.it>
2024-02-06 10:30:52 +0100

    create file storage in init

--- gin-gonic/main.go
+++ gin-gonic/main.go
@@ -1,13 +1,25 @@
 package main
 
 import (
+	"os"
+
 	"example.com/gin-gonic/api"
 	"example.com/gin-gonic/model"
 )
 
 func main() {
-	model.InitConfig("configs/config_dev.json")
-
 	router := api.SetupRouter()
 	router.Run("127.0.0.1:8080")
+}
+
+func init() {
+	if _, err := os.Stat(model.PlayersFilePath); os.IsNotExist(err) {
+		os.Create(model.PlayersFilePath)
+	}
+
+	if _, err := os.Stat(model.VersionFilePath); os.IsNotExist(err) {
+		os.Create(model.VersionFilePath)
+	}
+
+	model.InitConfig("configs/config_dev.json")
 }
