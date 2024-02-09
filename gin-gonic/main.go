4aefac0afc05bc3d34057fa7e6968fee6be6078c
Alberto Orsini <alberto.orsini@office.mailup.it>
2024-02-09 09:30:21 +0100

    add setup folder at startup

--- gin-gonic/main.go
+++ gin-gonic/main.go
@@ -1,6 +1,10 @@
 package main
 
 import (
+	"fmt"
+	"os"
+	"path/filepath"
+
 	"example.com/gin-gonic/api"
 	"example.com/gin-gonic/model"
 )
@@ -11,5 +15,16 @@ func main() {
 }
 
 func init() {
+
 	model.InitConfig("configs/config_dev.json")
+
+	if err := os.MkdirAll(filepath.Dir(model.PlayersFilePath), os.ModePerm); err != nil {
+		fmt.Println("error creating player path", err)
+		os.Exit(1)
+	}
+
+	if err := os.MkdirAll(filepath.Dir(model.VersionFilePath), os.ModePerm); err != nil {
+		fmt.Println("error creating version path", err)
+		os.Exit(1)
+	}
 }
